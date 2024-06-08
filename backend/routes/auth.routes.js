fad0652afb3e08a64062d5596c2fd0680877cd8e
vivek <vivekjha07777@gmail.com>
2024-06-08 12:40:12 +0530

    implement database connection and user_model_schema

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { signup, login, logout } from '../controllers/auth.controllers';
+import { signup, login, logout } from '../controllers/auth.controllers.js';
 const router= express.Router();
 
 
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,16 @@
 import express from 'express';
-const app= express();
+import dotenv from "dotenv";
 import authRoutes from './routes/auth.routes.js'
+import connectMongoDB from './db/connectMongoDB.js';
 
+dotenv.config();
+const app= express();
+const PORT = process.env.PORT || 5000;
+
+//...........authentication routes............
 app.use("/api/auth",authRoutes);
 
-app.listen(8000,()=>{
-    console.log("server is running on this port")
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+    connectMongoDB();
 })
