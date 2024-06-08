003bb2d19741a8d473a05e9c1eebec4d21df9c2e
vivek <vivekjha07777@gmail.com>
2024-06-08 20:48:13 +0530

    implement follow-unfollow logic suggestedUser logic and Edit profile logic

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,23 @@
 import express from 'express';
 import dotenv from "dotenv";
-import authRoutes from './routes/auth.routes.js'
-import connectMongoDB from './db/connectMongoDB.js';
 import cookieParser from 'cookie-parser';
+import { v2 as cloudinary } from "cloudinary"
+
+import authRoutes from './routes/auth.routes.js'
+import userRoutes from './routes/user.routes.js';
+
+import connectMongoDB from './db/connectMongoDB.js';
+
 
 dotenv.config();
+
+//................CONFIG CLOUDINARY ACCOUNT.................
+cloudinary.config({
+    cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
+    api_key:process.env.CLOUDINARY_API_KEY,
+    api_secret:process.env.CLOUDINARY_API_SECRET,
+});
+
 const app= express();
 const PORT = process.env.PORT || 5000;
 
@@ -16,6 +29,9 @@ app.use(cookieParser()); // to parse the cookie from request
 
 //...........authentication routes............
 app.use("/api/auth",authRoutes);
+
+//..........user Routes........................
+app.use("/api/users",userRoutes);
 
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
