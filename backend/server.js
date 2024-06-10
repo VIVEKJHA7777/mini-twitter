f7895b89c39e52e2a0eff966e581b7af56519795
vivek <vivekjha07777@gmail.com>
2024-06-10 22:37:34 +0530

    change in server.js file

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,19 @@
-import express from 'express';
 import path from "path";
+import express from "express";
 import dotenv from "dotenv";
-import cookieParser from 'cookie-parser';
-import { v2 as cloudinary } from "cloudinary"
+import cookieParser from "cookie-parser";
+import helmet from "helmet"; // Import helmet
+import { v2 as cloudinary } from "cloudinary";
 
-import authRoutes from './routes/auth.route.js'
-import userRoutes from './routes/user.route.js';
-import postRoutes from './routes/post.route.js';
-import notificationsRoutes from './routes/notification.route.js';
+import authRoutes from "./routes/auth.route.js";
+import userRoutes from "./routes/user.route.js";
+import postRoutes from "./routes/post.route.js";
+import notificationRoutes from "./routes/notification.route.js";
 
-import connectMongoDB from './db/connectMongoDB.js';
+import connectMongoDB from "./db/connectMongoDB.js";
 
 dotenv.config();
 
-// CONFIG CLOUDINARY ACCOUNT
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
@@ -24,25 +24,21 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
-// Middlewares
+app.use(helmet()); // Use helmet middleware to set security headers
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-// Authentication routes
 app.use("/api/auth", authRoutes);
-
-// User routes
 app.use("/api/users", userRoutes);
-
-// Post routes
 app.use("/api/posts", postRoutes);
-
-// Notifications routes
-app.use("/api/notifications", notificationsRoutes);
+app.use("/api/notifications", notificationRoutes);
 
 if (process.env.NODE_ENV === "production") {
+    // Serve static files from the frontend/dist directory
     app.use(express.static(path.join(__dirname, "frontend", "dist")));
+
+    // Serve index.html for all routes
     app.get("*", (req, res) => {
         res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
     });
